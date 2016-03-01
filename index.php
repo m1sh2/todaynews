421a483e138e1594b8d9190afc02eb914e6dce02
Mykhailo Datsko <misha@datsko.it>
2016-03-01 23:40:00 +0200

    Google Analytics code fix

--- index.php
+++ index.php
@@ -84,6 +84,18 @@ session_start();
   
   
   <script src="https://code.angularjs.org/2.0.0-alpha.50/angular2.min.js"></script> -->
+  
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-74505552-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
 </head>
 <body>
 <div class="container" ng-controller="MainCtrl as main">
